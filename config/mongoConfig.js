b5663efcb7bf4891c0f7b783ad1792d8ce1c2f6e
TimoteoGambella <timi.gambella@hotmail.com>
2023-06-26 11:30:55 -0300

    prueba2

--- config/mongoConfig.js
+++ config/mongoConfig.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+// const envConfig = require('./envConfig');
+const logger = require('../utils/logger');
 
+require('dotenv').config() 
     const connection = mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
     mongoose.connection.once('open', () => {
-        console.log(`MongoDB conectado`)
+        logger.info(`MongoDB conectado`)
     })
     module.exports = {connection}
