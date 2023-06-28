527f78fbe2128031019cf6031e6e76ea2559232c
TimoteoGambella <timi.gambella@hotmail.com>
2023-06-28 14:05:16 -0300

    cors

--- app.js
+++ app.js
@@ -23,12 +23,7 @@ const httpServer = new HttpServer(app);
     app.use(express.static(path.join(__dirname, 'public')))
     app.use(express.urlencoded({extended: true}))
     app.use(express.json())
-    app.use(cors({
-        origin: "*",
-        methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-        allowedHeaders: ["Access-Control-Allow-Origin", "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
-        credentials: true
-    }))
+    app.options(cors());
     app.use(session({
         secret: 'secret',
         resave: true,
